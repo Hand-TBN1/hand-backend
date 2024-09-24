a380c0330db699af18c88bf981254347d0ba435d
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-09-24 10:38:57 +0700

    feat: add postgres config

---
+++ config/postgresql.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"fmt"
+	"gorm.io/gorm/logger"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewPostgresql(migrations ...any) *gorm.DB {
+	gormLogger := logger.Default
+	if Env.ENV != "production" {
+		gormLogger = gormLogger.LogMode(logger.Info)
+	}
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			Env.PostgresHost,
+			Env.PostgresUser,
+			Env.PostgresPassword,
+			Env.PostgresDbName,
+			Env.PostgresPort,
+		),
+		PreferSimpleProtocol: true, // disables implicit prepared statement usage
+	}), &gorm.Config{
+		Logger: gormLogger,
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	if err := migratePostgresqlTables(db, migrations...); err != nil {
+		log.Fatalln(err)
+	}
+
+	return db
+}
+
+func migratePostgresqlTables(db *gorm.DB, migrations ...any) error {
+
+	if err := db.AutoMigrate(
+		migrations..., // BREAKING: entities should be passed from cmd/api/main.go due to circular dependency issue
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
