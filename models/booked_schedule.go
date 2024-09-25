e947b4ec37e0817013b264f0da29cecf6a439f67
AlbertChoe <albert.choe73@gmail.com>
2024-09-25 17:19:16 +0700

    fix enum

--- models/booked_schedule.go
+++ models/booked_schedule.go
@@ -6,12 +6,19 @@ import (
 	"github.com/google/uuid"
 )
 
+type BookedScheduleStatus string
+
+const (
+    Success  BookedScheduleStatus = "success"
+    Canceled BookedScheduleStatus = "canceled"
+)
+
 type BookedSchedule struct {
     ID              uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
     UserID          uuid.UUID `gorm:"type:uuid;not null"`
     TherapistID     uuid.UUID `gorm:"type:uuid;not null"`
     AppointmentDate time.Time
-    Status          string
+    Status         BookedScheduleStatus `gorm:"type:booked_schedule_status_enum"`
     UpdatedAt       time.Time
 
     // Associations
