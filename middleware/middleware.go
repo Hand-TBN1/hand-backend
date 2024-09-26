ce82d5c01fbe1c78c990d929e8f998e86c61c4a7
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-09-26 20:19:54 +0700

    feat: add auth middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -64,6 +64,50 @@ func RoleMiddleware(allowedRoles ...string) gin.HandlerFunc {
 }
 
 
+func Authenticate(allowedRoles ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Extract the token from the Authorization header
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, apierror.NewApiErrorBuilder().
+				WithStatus(http.StatusUnauthorized).
+				WithMessage("Authorization header missing").
+				Build())
+			c.Abort()
+			return
+		}
+
+		// Split "Bearer token"
+		tokenParts := strings.Split(authHeader, " ")
+		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, apierror.NewApiErrorBuilder().
+				WithStatus(http.StatusUnauthorized).
+				WithMessage("Invalid token format").
+				Build())
+			c.Abort()
+			return
+		}
+
+		// Validate the token
+		claims, err := utilities.ValidateJWT(tokenParts[1])
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, apierror.NewApiErrorBuilder().
+				WithStatus(http.StatusUnauthorized).
+				WithMessage("Invalid token").
+				Build())
+			c.Abort()
+			return
+		}
+
+		// Check if the user's role is allowed
+		c.Set("user.id", claims.UserID)
+		c.Set("user.name", claims.Name)
+		c.Set("user.role", claims.Role)
+		c.Next()
+	}
+}
+
+
 // Contoh Penggunaan
 // router.Use(middleware.RoleMiddleware("admin", "therapist")) // Allow only admin and therapist
 // 	{
