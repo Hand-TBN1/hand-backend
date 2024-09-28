482a166eb69c16fd26f8c746d1fec4e5b81e9364
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-09-28 23:31:24 +0700

    feat: add payment status

--- models/medication.go
+++ models/medication.go
@@ -9,13 +9,13 @@ import (
 type Medication struct {
     ID               uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
     ImageURL         string   `json:"image_url"`
-    Stock            int
-    Name             string
-    Price            int64
-    Description      string
-    RequiresPrescription bool
-    CreatedAt        time.Time
-    UpdatedAt        time.Time
+    Stock            int      `json:"stock"`
+    Name             string `json:"name"`
+    Price            int64 `json:"price"`
+    Description      string `json:"description"`
+    RequiresPrescription bool `json:"requiresPrescription"`
+    CreatedAt        time.Time `json:"createdAt"`
+    UpdatedAt        time.Time `json:"updatedAt"`
 
     // Associations
     Prescriptions    []Prescription    `gorm:"foreignKey:MedicationID"`
