74ae5b12c93bb9f3ae39fc17bfb15cf84bae1267
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-09-29 00:45:30 +0700

    feat: appointment controller

--- controller/appointment_controller.go
+++ controller/appointment_controller.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Hand-TBN1/hand-backend/apierror"
 	"github.com/Hand-TBN1/hand-backend/models"
 	"github.com/Hand-TBN1/hand-backend/services"
 	"github.com/Hand-TBN1/hand-backend/utilities"
@@ -13,6 +14,8 @@ import (
 
 type AppointmentController struct {
 	AppointmentService *services.AppointmentService
+	PaymentService *services.PaymentService
+	TherapistService *services.TherapistService
 }
 
 // CreateAppointment - Book an appointment with the therapist
@@ -43,12 +46,19 @@ func (ctrl *AppointmentController) CreateAppointment(c *gin.Context) {
 
 	userClaims := claims.(*utilities.Claims)
 	defaultStatus := models.Success
+
+	therapis , err := ctrl.TherapistService.GetTherapistDetails(userClaims.UserID)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Internal Server Error"})
+		return
+	}
 	// Create Appointment
 	appointment := models.Appointment{
 		ID:              uuid.New(),
 		UserID:          uuid.MustParse(userClaims.UserID),  // Get user_id directly from claims
 		TherapistID:     uuid.MustParse(req.TherapistID),
 		AppointmentDate: appointmentDate,
+		Price:  therapis.AppointmentRate,
 		Type:            models.ConsultationType(req.ConsultationType),
 		Status:          defaultStatus,
 		CreatedAt:       time.Now(),
@@ -59,6 +69,17 @@ func (ctrl *AppointmentController) CreateAppointment(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Appointment created successfully"})
+	paymentResponse, err := ctrl.PaymentService.CreatePayment(appointment.ID.String(), appointment.Price)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, apierror.NewApiErrorBuilder().
+            WithStatus(http.StatusInternalServerError).
+            WithMessage(err.Error()).
+            Build())
+        return
+    }
+
+
+
+	c.JSON(http.StatusOK, gin.H{"message": "Appointment created successfully", "redirect_url" : paymentResponse.RedirectURL})
 }
 
