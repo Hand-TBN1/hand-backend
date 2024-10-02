097a50e46e299e18e41ff5e0b64bc424c8234b6d
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-10-02 17:06:46 +0700

    fix: cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,15 +6,32 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Header("Access-Control-Allow-Origin", "http://localhost:3000")
-		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		ctx.Header("Access-Control-Allow-Credentials", "true") // Correct header for credentials
+		allowedOrigins := []string{
+			"http://localhost:3000",
+			"https://hand.tbn1.site",  
+		}
+
+		origin := ctx.Request.Header.Get("Origin")
+		var isAllowed bool
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin {
+				isAllowed = true
+				break
+			}
+		}
+
+		if isAllowed {
+			ctx.Header("Access-Control-Allow-Origin", origin) // Dynamically set allowed origin
+			ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+			ctx.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			ctx.Header("Access-Control-Allow-Credentials", "true") // Correct header for credentials
+		}
 
 		if ctx.Request.Method == "OPTIONS" {
 			ctx.AbortWithStatus(204)
 			return
 		}
+
 		ctx.Next()
 	}
 }
